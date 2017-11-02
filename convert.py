90d215c51f3ec54128badfc09bbcdbea8f1122a8
Andrey Shimanovich <strikebgu84@mail.ru>
2017-11-02 22:11:48 +0300

    add list files and date

--- convert.py
+++ convert.py
@@ -1,6 +1,8 @@
 import argparse
 import os
+import datetime
 
+# Работа с аргументами командной строки
 parser = argparse.ArgumentParser(prog='Convert pictures and docs to article', description='This is help')
 
 def readable_dir(prospective_dir):
@@ -15,4 +17,14 @@ parser.add_argument('-p', '--path', help='path to dir with input files', type=re
 args = parser.parse_args()
 paramerts = vars(args)
 path = paramerts['path']
-print(path)
+
+# Список файлов в дирректории
+list_files = []
+for entry in os.scandir(path):
+   if not entry.name.startswith('.') and entry.is_file():
+       list_files.append(entry.name)
+
+# Текущая дата
+date_today = datetime.date.today().strftime("%d_%m_%Y")
+
+
